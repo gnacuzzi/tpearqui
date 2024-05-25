11735b9ddb7727177f227f8af1304c507485305c
Gianna Nacuzzi <giannanacuzzi@MacBook-Pro-de-Gianna.local>
2024-05-25 19:56:44 -0300

    falta poder pasar el valor del los registros a strirng

--- Kernel/exceptions.c
+++ Kernel/exceptions.c
@@ -9,17 +9,21 @@ invalid_op_code( uint64_t * rip, uint64_t * rsp);
 static char * _regNames[] = {"RAX", "RBX", "RCX", "RDX", "RBP", "RDI", "RSI", "R8", "R9", "R10", "R11", "R12", "R13", "R14", "R15"};
 
 void print_regs(uint64_t rip, uint64_t rsp, uint64_t * newRsp) {
-	//para que funcione esto necesito tener printf
+	//creo que itoa hace cualquier cosa
+	char buffer[100];
 	draw_string("RIP: ");
-	draw_char('\n');
+	//draw_string(int_to_string(rip, buffer, 10));
+	draw_string("\n");
 	draw_string("RSP: ");
-	draw_char('\n');
+	//draw_string(int_to_string(rsp, buffer, 10));
+	draw_string("\n");
 	for (int i = 0; i < sizeof(_regNames)/sizeof(char *); i++){
 		draw_string(_regNames[i]);
 		draw_char(':');
 		draw_char(' ');
+		//draw_string(int_to_string(newRsp[i], buffer, 10));
+		draw_char('\n');
 	}
-	
 }
 
 void exceptionDispatcher(int exception, uint64_t * rip, uint64_t * rsp) {
@@ -32,6 +36,7 @@ void exceptionDispatcher(int exception, uint64_t * rip, uint64_t * rsp) {
 
 zero_division(uint64_t * rip, uint64_t * rsp) {
 	draw_string("\nerror: se lanzo una excepcion de division por zero\n");
+	draw_char('\n');
 	print_regs(rip, *rsp, rsp - 4);
 	draw_string("\nse reiniciara la consola");
 	draw_char('\n');
