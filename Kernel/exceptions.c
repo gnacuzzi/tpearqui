7e2250da9cce71169270b886cac2bc741dbbcfc8
Gianna Nacuzzi <giannanacuzzi@MacBook-Pro-de-Gianna.local>
2024-05-25 19:39:54 -0300

    falta poder programar itoa en el kernel

--- Kernel/exceptions.c
+++ Kernel/exceptions.c
@@ -1,29 +1,46 @@
 #include <videoDriver.h>
+#include <lib.h>
 #define ZERO_EXCEPTION_ID 0
 #define INVALID_OP_CODE_ID 6
 
-static void zero_division();
-static void invalid_op_code();
+zero_division(uint64_t * rip, uint64_t * rsp);
+invalid_op_code( uint64_t * rip, uint64_t * rsp);
 
-void exceptionDispatcher(int exception) {
+static char * _regNames[] = {"RAX", "RBX", "RCX", "RDX", "RBP", "RDI", "RSI", "R8", "R9", "R10", "R11", "R12", "R13", "R14", "R15"};
+
+void print_regs(uint64_t rip, uint64_t rsp, uint64_t * newRsp) {
+	//para que funcione esto necesito tener printf
+	draw_string("RIP: ");
+	draw_char('\n');
+	draw_string("RSP: ");
+	draw_char('\n');
+	for (int i = 0; i < sizeof(_regNames)/sizeof(char *); i++){
+		draw_string(_regNames[i]);
+		draw_char(':');
+		draw_char(' ');
+	}
+	
+}
+
+void exceptionDispatcher(int exception, uint64_t * rip, uint64_t * rsp) {
 	if (exception == ZERO_EXCEPTION_ID){
-		zero_division();
+		zero_division(rip,  rsp);
 	} else if (exception == INVALID_OP_CODE_ID){
-		invalid_op_code();
+		invalid_op_code( rip, rsp);
 	}
 }
 
-static void zero_division() {
+zero_division(uint64_t * rip, uint64_t * rsp) {
 	draw_string("\nerror: se lanzo una excepcion de division por zero\n");
-	//print_regs();
+	print_regs(rip, *rsp, rsp - 4);
 	draw_string("\nse reiniciara la consola");
 	draw_char('\n');
 	draw_char('\n');
 }
 
-void invalid_op_code() {
+invalid_op_code( uint64_t * rip, uint64_t * rsp) {
 	draw_string("\nerror: se lanzo una excepcion de operacion invalida\n");
-	//print_regs();
+	print_regs(rip, *rsp, rsp - 4);
 	draw_string("\nse reiniciara la consola");
 	draw_char('\n');
 	draw_char('\n');
