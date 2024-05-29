dc141cd15ba17713fd0adadd6e37e81b4ada636d
Agostina Squillari <msquillari@itba.edu.ar>
2024-05-29 08:44:39 -0300

    xd

--- run.sh
+++ run.sh
@@ -8,7 +8,8 @@ if [ "$OS" == "Darwin" ]; then
     qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev coreaudio,id=audio0 -machine pcspk-audiodev=audio0 
 
 elif [ "$OS" == "Linux" ]; then
-git else
+    qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev sdl,id=audio0 -machine pcspk-audiodev=audio0
+else
     echo "Sistema operativo no soportado"
     exit 1
 fi
