44f758a7b11f8c849c359e32330c538a0c2190eb
Agostina Squillari <msquillari@itba.edu.ar>
2024-06-04 18:03:49 -0300

    saque el sonido para linuz

--- run.sh
+++ run.sh
@@ -7,9 +7,7 @@ if [ "$OS" == "Darwin" ]; then
     # macOS - usa CoreAudio
     qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev coreaudio,id=audio0 -machine pcspk-audiodev=audio0 
 
-elif [ "$OS" == "Linux" ]; then
-    qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev sdl,id=audio0 -machine pcspk-audiodev=audio0
 else
-    echo "Sistema operativo no soportado"
-    exit 1
+    qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 
+
 fi
